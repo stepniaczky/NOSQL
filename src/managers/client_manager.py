ef2e17739b0514890a47d2d8e074fe54e73bde72
Jacek Nowak <jaceknowak226@gmail.com>
2022-10-19 23:39:56 +0200

    Get client refactor

--- src/managers/client_manager.py
+++ src/managers/client_manager.py
@@ -1,20 +1,20 @@
 from typing import List
-from src.models import Client, Address, ClientType
+from src.models import Client, Address
 
-from sqlalchemy import select, delete
 from sqlalchemy.engine import Row
+
 
 class ClientManager:
     user_table = Client.__tablename__
-    
+
     def __init__(self, sessionmaker):
         self.sessionmaker = sessionmaker
-    
+
     def add_client(self, client_id, first_name, last_name, birth_date, is_premium, city, street, number):
         # cliet_type = ClientType(birth_date)
         # address_id = None
         print("CFFFFFFFFFFFFFFFFFFF")
-        
+
         with self.sessionmaker() as session:
             addresses = session.query(Address).all()
             print(addresses)
@@ -26,10 +26,9 @@ class ClientManager:
             # address_id = address.id
             # except Exception as e:
             #     session.rollback()
-                
 
         # client = Client(client_id, first_name, last_name, birth_date, is_premium, )
-        
+
         # with self.sessionmaker() as session:
         #     try:
         #         client_type_id = ClientType.query.filter_by(type=cliet_type.type).one().id
@@ -38,7 +37,7 @@ class ClientManager:
         #         session.commit()
         #     except Exception as e:
         #         session.rollback()
-        
+
     def remove_client(self, client_id):
         with self.sessionmaker() as session:
             try:
@@ -47,13 +46,12 @@ class ClientManager:
                 session.commit()
             except Exception as e:
                 session.rollback()
-        
-        
+
     def get_client(self, client_id):
         with self.sessionmaker() as session:
-            response: Row = session.query(Client).filter(client_id=client_id).first()
-            return response
-        
+            client = session.query(Client).get(client_id)
+            return client
+
     def get_all_clients(self):
         with self.sessionmaker() as session:
             response: List[Row] = session.query(Client).all()
