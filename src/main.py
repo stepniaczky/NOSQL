49492c4dda941db594efacc496b33322eb46e68b
Jacek Nowak <jaceknowak226@gmail.com>
2022-10-19 22:21:07 +0200

    Get ticket method

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from db import get_session, init_db, get_engine_from_env
 from src.managers.movie_manager import MovieManager
+from src.managers.ticket_manager import TicketManager
 
 engine = get_engine_from_env()
 init_db(engine)
 session = get_session(engine)
 
 movieManager = MovieManager(session)
+ticketManager = TicketManager(session)
---
+++ src/managers/ticket_manager.py
@@ -0,0 +1,15 @@
+from src.models import Ticket
+
+
+class TicketManager:
+    def __init__(self, session):
+        self.session = session
+
+    def get_ticket(self, ticket_id):
+        with self.session() as session:
+            ticket = session.query(Ticket).get(ticket_id)
+
+            if ticket is None:
+                print('Film o takim id nie istnieje!')
+
+            return ticket
